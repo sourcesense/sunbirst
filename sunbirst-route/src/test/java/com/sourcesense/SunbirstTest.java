4dac363fddaf368f000759ec42e5ed8ec993c86b
Alex Cowell <alxcwll@gmail.com>
2011-09-29 16:06:48 +0100

    Restructured project layout and began work on a Solr component.

---
+++ sunbirst-route/src/test/java/com/sourcesense/SunbirstTest.java
@@ -0,0 +1,47 @@
+package com.sourcesense;
+
+import org.apache.camel.EndpointInject;
+import org.apache.camel.Produce;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.test.junit4.CamelTestSupport;
+import org.junit.Test;
+
+public class SunbirstTest extends CamelTestSupport {
+
+    @EndpointInject(uri = "mock:result")
+    protected MockEndpoint resultEndpoint;
+
+    @Produce(uri = "direct:start")
+    protected ProducerTemplate template;
+
+    @Test
+    public void testSendMatchingMessage() throws Exception {
+        String expectedBody = "<matched/>";
+
+        resultEndpoint.expectedBodiesReceived(expectedBody);
+
+        template.sendBodyAndHeader(expectedBody, "foo", "bar");
+
+        resultEndpoint.assertIsSatisfied();
+    }
+
+    @Test
+    public void testSendNotMatchingMessage() throws Exception {
+        resultEndpoint.expectedMessageCount(0);
+
+        template.sendBodyAndHeader("<notMatched/>", "foo", "notMatchedHeaderValue");
+
+        resultEndpoint.assertIsSatisfied();
+    }
+
+    @Override
+    protected RouteBuilder createRouteBuilder() {
+        return new RouteBuilder() {
+            public void configure() {
+                from("direct:start").filter(header("foo").isEqualTo("bar")).to("mock:result");
+            }
+        };
+    }
+}
---
+++ sunbirst-solr/src/main/java/com/sourcesense/sunbirst/component/solr/SolrComponent.java
@@ -0,0 +1,17 @@
+package com.sourcesense.sunbirst.component.solr;
+
+import org.apache.camel.Endpoint;
+import org.apache.camel.impl.DefaultComponent;
+
+import java.util.Map;
+
+/**
+ *
+ */
+public class SolrComponent extends DefaultComponent
+{
+    @Override
+    protected Endpoint createEndpoint(String s, String s1, Map<String, Object> stringObjectMap) throws Exception {
+        return null;
+    }
+}
